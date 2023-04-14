d7a8dc691cd59bfdc3050b78985453a5d3340420
Mac <mac@myauthenticwallet.com>
2023-04-14 11:58:24 -0400

    This is an automated change taken by Ema!

--- main/utils.py
+++ main/utils.py
@@ -1,9 +1,16 @@
+Update:
+
 from PIL import Image
+
+to:
+
+from PIL import Image, ImageOps
 
 
 def identify_image(image_path):
     try:
         with Image.open(image_path) as im:
-            return f"{image_path} {im.format} {im.size}x{im.mode}"
+            inverted_image = ImageOps.invert(im)
+            return f"{image_path} {inverted_image.format} {inverted_image.size[0]}x{inverted_image.size[1]} {inverted_image.mode}"
     except OSError:
         pass
