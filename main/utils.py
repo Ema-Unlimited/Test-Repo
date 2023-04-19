0f771585a15b5954d1b22b237af7f754e791f896
Mac <mac@myauthenticwallet.com>
2023-04-19 16:50:57 -0400

    This is an automated change taken by Ema!

--- main/utils.py
+++ main/utils.py
@@ -4,6 +4,7 @@ from PIL import Image
 def identify_image(image_path):
     try:
         with Image.open(image_path) as im:
-            return f"{image_path} {im.format} {im.size}x{im.mode}"
+            return f"{image_path} {im.format} {im.size[0]}x{im.size[1]} {im.mode}"
     except OSError:
         pass
+
